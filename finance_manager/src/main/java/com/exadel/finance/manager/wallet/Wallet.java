3fda9f4d3db6caa0a6098fe852ab98cdb18b3cae
AlexPravda1 <kabinkos@gmail.com>
2022-07-22 12:55:26 +0400

    test wallets DAO and endpoints

---
+++ finance_manager/src/main/java/com/exadel/finance/manager/wallet/Wallet.java
@@ -0,0 +1,32 @@
+package com.exadel.finance.manager.wallet;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Entity
+@Table(name = "wallets")
+@Getter
+@Setter
+@NoArgsConstructor
+@SQLDelete(sql = "UPDATE wallets SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
+@ToString
+public class Wallet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false, nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+}
---
+++ finance_manager/src/main/java/com/exadel/finance/manager/wallet/WalletController.java
@@ -0,0 +1,35 @@
+package com.exadel.finance.manager.wallet;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/wallets")
+@RequiredArgsConstructor
+public class WalletController {
+    private final WalletRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<Wallet>> getAllWalltes() {
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Wallet> getWalletById(@PathVariable(name = "id") Long id) {
+        Wallet wallet = new Wallet();
+        wallet.setName("NO SUCH WALLET");
+        return ResponseEntity.ok(repository.findById(id).orElse(wallet));
+    }
+
+    @PutMapping
+    public ResponseEntity<Wallet> createWallet(@RequestBody Wallet wallet) {
+        return ResponseEntity.ok(repository.save(wallet));
+    }
+}
