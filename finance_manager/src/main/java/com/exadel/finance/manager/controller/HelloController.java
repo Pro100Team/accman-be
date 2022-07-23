93e38ed1286d690aa6d3722f12d3ca5000864ebf
AlexPravda1 <kabinkos@gmail.com>
2022-07-23 10:51:20 +0400

    test autodeply

--- finance_manager/src/main/java/com/exadel/finance/manager/controller/HelloController.java
+++ finance_manager/src/main/java/com/exadel/finance/manager/controller/HelloController.java
@@ -12,6 +12,6 @@ public class HelloController {
 
     @GetMapping
     public ResponseEntity<String> getHelloExadel() {
-        return new ResponseEntity<>("Hello Exadel !!! (test)", HttpStatus.OK);
+        return new ResponseEntity<>("Hello Exadel !!! (test2)", HttpStatus.OK);
     }
 }
