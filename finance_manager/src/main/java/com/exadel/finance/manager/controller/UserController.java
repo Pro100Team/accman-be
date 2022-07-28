bc39f7d2cc3e7131bf88a4cadb4ded30da345606
GapSerg <gapserg@tut.by>
2022-07-28 22:25:06 +0200

    Fix checkstyle

--- finance_manager/src/main/java/com/exadel/finance/manager/controller/UserController.java
+++ finance_manager/src/main/java/com/exadel/finance/manager/controller/UserController.java
@@ -5,21 +5,17 @@
 
 package com.exadel.finance.manager.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.context.request.NativeWebRequest;
-
-import java.util.Optional;
 import org.example.api.UserApi;
 import org.example.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
 @Controller
 public class UserController implements UserApi {
 
-
     @Override
     public ResponseEntity<String> loginUser(User user) {
-          return ResponseEntity.ok("!!! "+user.getLogin());
-
+        return ResponseEntity.ok("!!! " + user.getLogin());
     }
 }
 
