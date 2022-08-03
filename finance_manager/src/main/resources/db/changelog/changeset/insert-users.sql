9037da2273f498152fe2531da9d43235215b32da
Grzegorz Sadowski <34543404+GrzegorzSadowski@users.noreply.github.com>
2022-08-03 20:37:58 +0200

    Update insert-users.sql

--- finance_manager/src/main/resources/db/changelog/changeset/insert-users.sql
+++ finance_manager/src/main/resources/db/changelog/changeset/insert-users.sql
@@ -5,4 +5,5 @@ INSERT INTO users (u_email, u_password, u_is_admin)
 VALUES ('Sergey@exadel.com', 'Sergey!2345', true),
        ('Katia@exadel.com', 'Katia!2345', false),
        ('Jura@exadel.com', 'Jura!2345', true),
-       ('Anna@exadel.com', 'Anna!2345', false);
+       ('Anna@exadel.com', 'Anna!2345', false)
+ON CONFLICT (u_email) DO NOTHING;
