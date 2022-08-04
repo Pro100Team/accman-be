1574666f19a8c1345f07f4f7e67115d700c3dfa6
AlexPravda1 <kabinkos@gmail.com>
2022-08-04 12:45:39 +0400

    - fix external dependancy

--- finance_manager/src/main/java/com/exadel/finance/manager/controller/HelloController.java
+++ finance_manager/src/main/java/com/exadel/finance/manager/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.exadel.finance.manager.controller;
 
-import com.sandbox.api.UsersApi;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
