a1f90aa1ce81c3521ef5576fdf27259d458362e9
AlexPravda1 <kabinkos@gmail.com>
2022-08-04 12:33:51 +0400

    - fix checkstyle

--- finance_manager/src/main/resources/db/changelog/changeset/insert-users.sql
+++ finance_manager/src/main/resources/db/changelog/changeset/insert-users.sql
@@ -6,4 +6,4 @@ VALUES ('Sergey@exadel.com', 'Sergey!2345', true),
        ('Katia@exadel.com', 'Katia!2345', false),
        ('Jura@exadel.com', 'Jura!2345', true),
        ('Anna@exadel.com', 'Anna!2345', false)
-ON CONFLICT (u_email) DO NOTHING;
+ON CONFLICT DO NOTHING;
