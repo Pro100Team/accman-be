cafd44cbc9414e78b20a53b1c1ad6849f6dc7684
AlexPravda1 <kabinkos@gmail.com>
2022-08-11 09:33:09 +0400

    - swagger doesn't need AUTH - swagger settings adjust  to include only RestControllers - Dockerfile amended - slight adjustments in wallet/user files

--- finance_manager/src/main/java/com/manager/finance/config/WebConfig.java
+++ finance_manager/src/main/java/com/manager/finance/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.manager.finance.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +12,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/images/**", "/js/**", "/webjars/**",
+                "/swagger-ui/*", "/swagger-ui.html", "/configuration/**",
+                "/swagger-resources/**", "/v2/api-docs");
     }
 }
