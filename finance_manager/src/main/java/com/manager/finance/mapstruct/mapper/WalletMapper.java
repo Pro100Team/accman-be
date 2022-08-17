910f80bebdd19fa32d535df4663810eb4ccac74b
GapSerg <gapserg@tut.by>
2022-08-17 15:34:30 +0200

    1. TransactionMapper and WalletMapper for correct work with amount

--- finance_manager/src/main/java/com/manager/finance/mapstruct/mapper/WalletMapper.java
+++ finance_manager/src/main/java/com/manager/finance/mapstruct/mapper/WalletMapper.java
@@ -3,15 +3,22 @@ package com.manager.finance.mapstruct.mapper;
 import com.manager.finance.wallet.model.entity.Wallet;
 import com.sandbox.model.WalletRequestDto;
 import com.sandbox.model.WalletResponseDto;
+import java.util.Formatter;
 import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface WalletMapper {
 
-    @Mapping(target = "balance", source = "amount")
+    @Named("intToStringDouble")
+    public static String intToStringDouble(int amount){
+        return new Formatter().format("%.2f", ((double)(amount))/100).toString();
+    }
+
+    @Mapping(target = "balance", source = "amount", qualifiedByName = "intToStringDouble")
     WalletResponseDto walletToWalletResponseDto(Wallet wallet);
 
     Wallet walletRequestDtoToWalletUpdate(@MappingTarget Wallet wallet,
@@ -19,6 +26,6 @@ public interface WalletMapper {
 
     Wallet walletRequestDtoToWallet(WalletRequestDto walletRequestDto);
 
-    @Mapping(target = "balance", source = "amount")
+    @Mapping(target = "balance", source = "amount", qualifiedByName = "intToStringDouble")
     List<WalletResponseDto> walletListToWalletResponseDtoList(List<Wallet> wallets);
 }
