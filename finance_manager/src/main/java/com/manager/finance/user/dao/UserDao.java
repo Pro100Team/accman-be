df90f283bdee4e302e97c919d4f24334b79d19b3
Ivan <ivankaminskii323@gmail.com>
2022-08-18 10:41:30 +0200

    Removed tests

--- finance_manager/src/main/java/com/manager/finance/user/dao/UserDao.java
+++ finance_manager/src/main/java/com/manager/finance/user/dao/UserDao.java
@@ -4,7 +4,7 @@ import com.manager.finance.user.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
-@Component
+
 public interface UserDao extends JpaRepository<User, Long> {
     User findUserByLogin(String login);
 }
